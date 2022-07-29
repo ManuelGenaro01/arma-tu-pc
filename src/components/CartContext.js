1b2d026ca480ba429ed9d08d7eca15a413282645
ManuelGenaro01 <manuelgenaro19@gmail.com>
2022-07-29 01:45:45 -0300

    Entrega Clase 9

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -4,14 +4,20 @@ export const CartContext = createContext();
 
 const CartContextProvider = ({children}) =>{
     const [cartValue, setCartValue] = useState([])
-    const addToCart = (item, cantidad) =>{
-        setCartValue([...cartValue, {
+    const addToCart = (item, nuevaCantidad) =>{
+        const {cantidad = 0}=cartValue.find(product => product.id === item.id) || [];
+        const nuevoCart = cartValue.filter(prod => prod.id!==item.id)
+        setCartValue([...nuevoCart, {
             id: item.id,
             nombre: item.nombre,
             img: item.img,
             precio: item.precio,
-            cantidad: cantidad
+            cantidad: cantidad + nuevaCantidad
         }])
+    }
+
+    const isInCart =(id)=>{
+        return cartValue.find(product => product.id === id) ? true:false;
     }
 
     const removeItem =(id)=>{
@@ -23,7 +29,7 @@ const CartContextProvider = ({children}) =>{
     }
 
     return(
-        <CartContext.Provider value={{cartValue, addToCart, removeItem, clear}}>
+        <CartContext.Provider value={{cartValue, addToCart, removeItem, clear, isInCart}}>
             {children}
         </CartContext.Provider>
     )
